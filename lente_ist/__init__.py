7ce9ca6dddc531d8f90fc83c3d41b19d5985bd10
higorsmonteiro <higormonteiros@gmail.com>
2024-08-20 08:04:20 -0300

    updt init

--- lente_ist/__init__.py
+++ lente_ist/__init__.py
@@ -1,5 +1,6 @@
 from .warehouse_model.warehouse_model import WarehouseIST
 from .warehouse_model.warehouse_injector import InjectorIST
 from .agents.deduple_agent import DedupleAgent
+from .data_matching.matching_data import Deduple, PLinkage
 from .process_layer.base import ProcessBase
 from .process_layer.sus_specific import ProcessSinan
